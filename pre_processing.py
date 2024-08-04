41883763e7ba42ee9fced8bb5b685d41c35b9e6b
ASAIs24 <20asais24@gmail.com>
2024-08-04 16:02:39 +0300

    Lemmatization işlemi bu koddan silindi. Daha ayrıntılı olması için ek kod yazdık  lemmatization.py dan erişim sağlayabilirsiniz.  Bağlantı tipi, renk, model gibi ekstra eklenen sütunlarda veri bulunmazsa nan olarak kayıt yapılmaktadır sonrasında etiketleme yapmadan önce manuel olarak biz düzeltmekteyiz.

--- pre_processing.py
+++ pre_processing.py
@@ -1,15 +1,7 @@
-
 import pandas as pd
 import re
 import numpy as np
 import emoji
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from collections import Counter
-import matplotlib.pyplot as plt
-import jpype
-from jpype import JClass, getDefaultJVMPath, startJVM
 
 ozel_karakterin_boşlukla_degismesi = True
 kucuk_harfe_cevirme = True
@@ -21,15 +13,29 @@ parantez_silme = True
 renk_bilgisi_cikarma = True
 model_bilgisi_cikarma = True
 baglanti_tipi_cikarma = True
-generate_word_frequency = False
 
-custom_stop_words_file = "stopWords.txt"
-data_excel = 'products.xlsx'
-temizlenmis_excel = 'sonuc.xlsx'
-labellamak_icin = 'labellamak_icin.csv'
+data_excel =  r" " #xlsx uzantılı data setinin path'ini buraya eklemek gerekmektedir 
+output = 'sonuc.xlsx' # çıktının dosya adını burdan belirleyebilirsiniz
 
+# Excel dosyasını oku
+df = pd.read_excel(data_excel)
+
+# Stop words listesini dosyadan okuma fonksiyonu
+def stop_words_cikarma(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        stop_words = set(line.strip() for line in file)
+    return stop_words
+    
+stop_words = stop_words_cikarma(r"C:\Users\beyza\Downloads\stopWords (5).txt")
+    
+# Stop words'leri kaldırma fonksiyonu
+def stop_word_silme(text, stop_words):
+    tokens = text.split()  # Cümleyi tokenlara ayır
+    filtered_tokens = [word for word in tokens if word not in stop_words]  # Stop words'leri kaldır
+    return ' '.join(filtered_tokens)  # Tokenları tekrar birleştir
+    
 # Özel karakterleri temizleme fonksiyonu
-def metni_tenizle(text, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme):
+def temizle_metni(text, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme):
     if ozel_karakterin_boşlukla_degismesi:
         text = re.sub(r'[^\w\s]', ' ', text)
     if kucuk_harfe_cevirme:
@@ -43,8 +49,8 @@ def metni_tenizle(text, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme
     return text
 
 # Fiyat sütunundaki TL'yi kaldırma fonksiyonu
-def fiyat_sutunu_tl_kaldirma(fiyat, tl_silme):
-    if tl_silme:
+def  fiyat_sutunu_tl_kaldirma(fiyat, remove_tl):
+    if remove_tl:
         return re.sub(r'TL', '', str(fiyat)).strip()
     return str(fiyat).strip()
 
@@ -72,78 +78,33 @@ def model(urun_adı, modeller, model_bilgisi_cikarma):
 
 def baglanti_tipi(urun_adı, baglanti_tipi_cikarma):
     if baglanti_tipi_cikarma:
-        if re.search(r'bluetooth|kablosuz', urun_adı, re.IGNORECASE):
+        if re.search(r'bluetooth|kablosuz|wireless', urun_adı, re.IGNORECASE):
             return "kablosuz"
         elif re.search(r'kablolu', urun_adı, re.IGNORECASE):
             return "kablolu"
     return np.nan
 
 
-# Zemberek ile kök bulma fonksiyonu(Lemmatization)
-def kok_bulma(tokens):
-    kelimelerin_kokleri = []
-    ZEMBEREK_PATH = r"zemberek-full_old.jar"
-    if not jpype.isJVMStarted():
-        startJVM(getDefaultJVMPath(), '-ea', '-Djava.class.path=%s' % (ZEMBEREK_PATH))
-    TurkishMorphology = JClass('zemberek.morphology.TurkishMorphology')
-    morphology = TurkishMorphology.createWithDefaults()
-    for token in tokens:
-        analysis = morphology.analyzeAndDisambiguate(str(token)).bestAnalysis()
-        kelimenin_koku = str(analysis[0].getLemmas()[0])
-        if kelimenin_koku == "UNK":
-            kelimenin_koku = token
-        kelimelerin_kokleri.append(kelimenin_koku)
-    return kelimelerin_kokleri
-
-# POS etiketleme fonksiyonu(sözcük türü)
-def pos_zemberek(tokens):
-    pos_etiketleri = []
-    ZEMBEREK_PATH = r"zemberek-full_old.jar"
-    if not jpype.isJVMStarted():
-        startJVM(getDefaultJVMPath(), '-ea', '-Djava.class.path=%s' % (ZEMBEREK_PATH))
-    TurkishMorphology = JClass('zemberek.morphology.TurkishMorphology')
-    morphology = TurkishMorphology.createWithDefaults()
-    for token in tokens:
-        analysis = morphology.analyzeAndDisambiguate(token).bestAnalysis()
-        if analysis:
-            best = analysis[0]
-            pos_etiketleri.append((token, best.getPos().shortForm))
-        else:
-            pos_etiketleri.append((token, 'UNK'))
-    return pos_etiketleri
-
-# Kelime frekanslarını hesaplayan ve görselleştiren fonksiyon
-def kelime_frekansi_ve_gorsellestirme(df):
-    all_tokens = [token for sublist in df['Stop Words Çıkarılmış Hali'] for token in sublist]
-    word_freq = Counter(all_tokens)
-    common_words = word_freq.most_common(20)
-    
-    print("En sık geçen kelimeler ve frekansları:")
-    for word, freq in common_words:
-        print(f"{word}: {freq}")
-
-    words, counts = zip(*common_words)
-    plt.figure(figsize=(12, 8))
-    plt.bar(words, counts)
-    plt.xlabel('Kelimeler')
-    plt.ylabel('Frekans')
-    plt.title('En Sık Geçen Kelimeler')
-    plt.xticks(rotation=45)
-    plt.show()
-
-# Excel dosyasını oku
-df = pd.read_excel(data_excel)
-
 # Renkler ve modeller için listeler
-renkler = [] #datasetindeki ürünlerin renklerinin listesi buraya eklenmelidir
-modeller = [] #datasetindeki modellerin listesi buraya eklenmelidir
+renkler = [ "siyah", "beyaz", "mavi", "kırmızı", "yeşil", "gri", "altın", "gümüş", "pembe", "mor", "turuncu", "sarı", "lacivert", "kahverengi"]
+
+
+modeller = ['airpods pro 2.nesil', 'airpods 3.nesil', 'airpods 3.nesil lightning', 'apple lightning konnektörlü', 'freebuds se', 'tune 510bt', 'tune 520bt', 'tune 560bt', 'tune 570bt', 'c100sı', 'tune 500', 'vibe 100 tws', 'wave 300 tws', 'tah4205', 'galaxy buds fe', 'eo-ia500b', 'galaxy buds 2', 'ehs61', 'redmi buds 4 active', 'redmi buds 4 lite', 'mi true wireless earphones 2 basic', 'piston basic edition', 'redmi buds 3 lite', 'redmi buds 3', 'redmi buds essential','tune t510bt']
+# Remove "devamını oku" and the preceding text up to the previous space
+df['Yorum'] = df['Yorum'].apply(lambda x: re.sub(r'\S*devamını oku', '', x, flags=re.IGNORECASE).strip())
+
 
 # Metin temizleme ve yeni sütunlar ekleme
-df['Yorum'] = df['Yorum'].apply(lambda x: metni_tenizle(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
-df['Ürün İsmi'] = df['Ürün İsmi'].apply(lambda x: metni_tenizle(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
-df['Ürün Marka'] = df['Ürün Marka'].apply(lambda x: metni_tenizle(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
-df['Satıcı Adı'] = df['Satıcı Adı'].apply(lambda x: metni_tenizle(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
-df['Yorum Satıcı Adı'] = df['Yorum Satıcı Adı'].apply(lambda x: metni_tenizle(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
+df['Yorum'] = df['Yorum'].apply(lambda x: temizle_metni(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
+
+# Stop words'leri kaldırma
+df['Yorum'] = df['Yorum'].apply(lambda x: stop_word_silme(x, stop_words))
+
+
+df['Ürün İsmi'] = df['Ürün İsmi'].apply(lambda x: temizle_metni(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
+df['Ürün Marka'] = df['Ürün Marka'].apply(lambda x: temizle_metni(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
+df['Satıcı Adı'] = df['Satıcı Adı'].apply(lambda x: temizle_metni(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
+df['Yorum Satıcı Adı'] = df['Yorum Satıcı Adı'].apply(lambda x: temizle_metni(x, ozel_karakterin_boşlukla_degismesi, kucuk_harfe_cevirme, newline_karakteri_boslukla_degistirme, fazla_boslukları_silme, emoji_silme))
 
 df['Fiyat'] = df['Fiyat'].apply(lambda x: fiyat_sutunu_tl_kaldirma(x, tl_silme))
 df['Beğeni Sayısı'] = df['Beğeni Sayısı'].apply(lambda x: begeni_sayisi_parantez_kaldirma(x, parantez_silme))
@@ -152,60 +113,18 @@ df['Renk'] = df['Ürün İsmi'].apply(lambda x: renk(x, renkler, renk_bilgisi_ci
 df['Model'] = df['Ürün İsmi'].apply(lambda x: model(x, modeller, model_bilgisi_cikarma))
 df['Bağlantı Tipi'] = df['Ürün İsmi'].apply(lambda x: baglanti_tipi(x, baglanti_tipi_cikarma))
 
+
+# stringe çevirme işlemi
+df['Renk'] = df['Renk'].astype(str)
+df['Model'] = df['Model'].astype(str)
+df['Bağlantı Tipi'] = df['Bağlantı Tipi'].astype(str)
+
 # Küçük harfe çevirme işlemi
 df['Renk'] = df['Renk'].str.lower()
 df['Model'] = df['Model'].str.lower()
 df['Bağlantı Tipi'] = df['Bağlantı Tipi'].str.lower()
 
-
 # Tamamen aynı olan satırlardan sadece bir tanesini bırak
 df = df.drop_duplicates()
 
-# Stop words verilerini indirme
-nltk.download('stopwords')
-nltk_stop_words = set(stopwords.words('turkish'))
-
-# Kendi stop words dosyasını okuma
-with open(custom_stop_words_file, 'r', encoding='utf-8') as f:
-    custom_stop_words = set(f.read().splitlines())
-
-# NLTK stop words ve kendi stop words'leri birleştirme
-combined_stop_words = nltk_stop_words.union(custom_stop_words)
-
-def tokenize_comment(comment):
-    tokens = word_tokenize(comment)
-    return tokens
-
-def stop_word_silme(tokens):
-    filtered_tokens = [word for word in tokens if word not in combined_stop_words]
-    return filtered_tokens
-
-# Yorumları tokenlarına ayır ve yeni bir sütuna ekle
-df['Yorum Tokenleri'] = df['Yorum'].apply(tokenize_comment)
-
-# Tokenların köklerini bul ve yeni bir sütuna ekle
-df['Köklerine Ayrılmış Yorumlar'] = df['Yorum Tokenleri'].apply(kok_bulma)
-
-# Stop words'leri çıkar ve yeni bir sütuna ekle
-df['Stop Words Çıkarılmış Hali'] = df['Köklerine Ayrılmış Yorumlar'].apply(stop_word_silme)
-
-# Zemberek ile POS etiketleme ve yeni sütun ekleme
-df['POS Etiketleri'] = df['Stop Words Çıkarılmış Hali'].apply(pos_zemberek)
-
-# Temizlenmiş ve tokenize edilmiş veri setini yeni bir Excel dosyasına kaydedin
-df.to_excel(temizlenmis_excel)
-
-# Kelime frekansı ve görselleştirme fonksiyonunu isteğe bağlı olarak çalıştır
-if generate_word_frequency:
-    kelime_frekansi_ve_gorsellestirme(df)
-
-# cümle halinde yazma fonksiyonu
-def tokens_to_sentence(tokens):
-    return ' '.join(tokens)
-
-# Yeni sütun: Token_Lemma_StopWords (cümle halinde)
-df['Token_Lemma_StopWords'] = df['Stop Words Çıkarılmış Hali'].apply(tokens_to_sentence)
-
-# labellamak_icin.csv dosyasını oluşturma
-labellamak_icin_df = df[['Yorum', 'Token_Lemma_StopWords']]
-labellamak_icin_df.to_csv(labellamak_icin, index=False)
+df.to_excel(output, index=False)
